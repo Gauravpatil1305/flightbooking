972fdd10732f9ce91601a2cc94f0d25ab3b24fae
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-09-16 21:38:28 +0300

    Update
    Updated for Navbar.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
+import { Icon } from "@iconify/react";
+import { NavbarItem } from "@/types/types";
 
-const Navbar: React.FC = () => {
+interface NavbarProps {
+  items: NavbarItem[];
+}
+
+const Navbar: React.FC<NavbarProps> = ({ items }) => {
   return (
-    <>
-      <h1>Navbar</h1>
-    </>
+    <nav>
+      <ul>
+        {items.map((item) => (
+          <li key={item.name}>
+            <Link href={item.path}>
+                <Icon icon={item.icon} />
+                <span>{item.name}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
---
+++ src/data/navbarData.ts
@@ -0,0 +1,11 @@
+import { NavbarItem } from "@/types/types";
+
+export const navbarData: NavbarItem[] = [
+    { name: "Home", icon: "material-symbols:home-pin-outline", path: "/" },
+    { name: "Ticket Management", icon: "material-symbols:airplane-ticket-outline-rounded", path: "/ticket-management"  },
+    { name: "Experience", icon: "material-symbols:workspace-premium-rounded", path: "#"  },
+    { name: "Campaign", icon: "material-symbols:campaign-rounded", path: "#"  },
+    { name: "Destination", icon: "material-symbols:flight-takeoff-rounded", path: "#"  },
+    { name: "Help", icon: "material-symbols:help-outline-rounded", path: "#"  },
+    { name: "Contact", icon: "material-symbols:headphones-outline-rounded", path: "#"  },
+];
