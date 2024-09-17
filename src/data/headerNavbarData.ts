121158fa00da51d34e86a93ae71c74f28f76ba43
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-09-17 20:48:38 +0300

    Update
    Updated for pages.

--- src/data/headerNavbarData.ts
+++ src/data/headerNavbarData.ts
@@ -3,9 +3,9 @@ import { HeaderNavbarItem } from "@/types/types";
 export const headerNavbarData: HeaderNavbarItem[] = [
     { name: "Home", icon: "material-symbols:home-pin-outline", path: "/" },
     { name: "Ticket Management", icon: "material-symbols:airplane-ticket-outline-rounded", path: "/ticket-management"  },
-    { name: "Experience", icon: "material-symbols:workspace-premium-rounded", path: "#"  },
-    { name: "Campaign", icon: "material-symbols:campaign-rounded", path: "#"  },
-    { name: "Destination", icon: "material-symbols:flight-takeoff-rounded", path: "#"  },
-    { name: "Help", icon: "material-symbols:help-outline-rounded", path: "#"  },
-    { name: "Contact", icon: "material-symbols:headphones-outline-rounded", path: "#"  },
+    { name: "Experience", icon: "material-symbols:workspace-premium-rounded", path: "/experience"  },
+    { name: "Campaign", icon: "material-symbols:campaign-rounded", path: "/campaign"  },
+    { name: "Destination", icon: "material-symbols:flight-takeoff-rounded", path: "/destination"  },
+    { name: "Help", icon: "material-symbols:help-outline-rounded", path: "/help"  },
+    { name: "Contact", icon: "material-symbols:headphones-outline-rounded", path: "/contact"  },
 ];
