408df304543bcfb714d829f7cf702f04eca4bccd
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-09-19 21:44:08 +0300

    Update
    Updated for Header.

--- src/components/Header/components/Account/Account.tsx
+++ src/components/Header/components/Account/Account.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Icon } from "@iconify/react";
 
 const Account = () => {
   return (
     <div className="account">
       <button>
+        <Icon icon="material-symbols:supervised-user-circle-outline" />
         <span>Sign In</span>
       </button>
     </div>
