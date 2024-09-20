59578096e9e72289382f7515dd96758cab207002
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-09-20 16:09:11 +0300

    Update
    Updated for Header.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+"use client";
 
+import React, { useState } from "react";
 import Logo from "./components/Logo/Logo";
 import Navbar from "./components/Navbar/Navbar";
 import Account from "./components/Account/Account";
 import { headerNavbarData } from "@/data/headerNavbarData";
 
 import "./styles/Header.scss";
+import { Icon } from "@iconify/react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="header">
       <div className="container content">
         <Logo />
-        <Navbar items={headerNavbarData} />
+        <button className="menu-btn" onClick={toggleMenu}>
+          <Icon icon="mdi:menu" />
+        </button>
+        <Navbar items={headerNavbarData} isOpen={isOpen} toggleMenu={toggleMenu} />
         <Account />
       </div>
     </header>
--- src/components/Header/components/Navbar/Navbar.tsx
+++ src/components/Header/components/Navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
 import { Icon } from "@iconify/react";
@@ -5,24 +7,32 @@ import { HeaderNavbarItem } from "@/types/types";
 
 interface NavbarProps {
   items: HeaderNavbarItem[];
+  isOpen: boolean;
+  toggleMenu: () => void;
 }
 
-const Navbar: React.FC<NavbarProps> = ({ items }) => {
+const Navbar: React.FC<NavbarProps> = ({ items, isOpen, toggleMenu }) => {
   return (
-    <div className="navbar">
-      <nav>
-        <ul>
-          {items.map((item) => (
-            <li key={item.name}>
-              <Link href={item.path}>
-                <Icon icon={item.icon} />
-                <span>{item.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </div>
+    <>
+      <div className={`navbar ${isOpen ? "open" : ""}`}>
+        <span className="close-btn" onClick={toggleMenu}>
+          <Icon icon="mdi:close" />
+        </span>
+        <nav>
+          <ul>
+            {items.map((item) => (
+              <li key={item.name}>
+                <Link href={item.path} onClick={toggleMenu}>
+                  <Icon icon={item.icon} />
+                  <span>{item.name}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+      {isOpen && <div className="overlay" onClick={toggleMenu}></div>}
+    </>
   );
 };
 
