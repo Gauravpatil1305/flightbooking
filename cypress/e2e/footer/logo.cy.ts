033b1bd08232bdc1f82b459fab81db56d608d16a
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-09-30 21:25:34 +0300

    Update
    Updated for Cypress Tests.

--- cypress/e2e/footer/logo.cy.ts
+++ cypress/e2e/footer/logo.cy.ts
@@ -1,17 +1,17 @@
-describe("Header Logo Component", () => {
+describe("Footer Logo Component", () => {
   beforeEach(() => {
     cy.visit("/"); // Navigate to the homepage before each test
   });
 
-  it("Should render the FlightRoutes logo in the header", () => {
-    cy.get(".header-logo img")
+  it("Should render the FlightRoutes logo in the footer", () => {
+    cy.get(".footer-logo img")
       .should("be.visible") // Verify the logo is visible
       .and("have.attr", "alt", "FlightRoutes Logo"); // Check if alt text is correct
   });
 
-  it("Should navigate to the homepage when clicking the header logo", () => {
+  it("Should navigate to the homepage when clicking the footer logo", () => {
     cy.visit("/contact"); // Visit another page
-    cy.get(".header-logo a").click(); // Click the logo in the header
+    cy.get(".footer-logo a").click(); // Click the logo in the footer
     cy.url().should("eq", Cypress.config().baseUrl + "/"); // Check redirection to the homepage
   });
 });
