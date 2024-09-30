d44890cbdcb5de7022f1f748d17ffdea08e2d017
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-09-30 20:40:38 +0300

    Update
    Updated Footer component for mobile screen.

--- src/components/Footer/components/Navbar/Navbar.tsx
+++ src/components/Footer/components/Navbar/Navbar.tsx
@@ -1,18 +1,35 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
 import { FooterNavbarItem } from "@/types/types";
+import { Icon } from '@iconify/react'; 
 
 interface NavbarProps {
   items: FooterNavbarItem[];
 }
 
 const Navbar: React.FC<NavbarProps> = ({ items }) => {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  const toggleAccordion = (index: number) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
   return (
     <div className="navbar">
-      {items.map((item) => (
+      {items.map((item, index) => (
         <div className="navbar-content" key={item.title}>
-          <h4 className="navbar-title">{item.title}</h4>
-          <ul className="navbar-list">
+          <h4
+            className="navbar-title"
+            onClick={() => toggleAccordion(index)}
+          >
+            {item.title}
+            <span className={`navbar-icon ${openIndex === index ? "open" : ""}`}>
+              <Icon icon="mingcute:down-fill" width={30} height={30} />
+            </span>
+          </h4>
+          <ul className={`navbar-list ${openIndex === index ? "open" : ""}`}>
             {item.links.map((link) => (
               <li className="navbar-item" key={link.name}>
                 <Link href={link.path}>
