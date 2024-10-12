323c7ee54ffd29c63a5f83720db4e309449fb7c6
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-10-12 22:40:39 +0300

    Update
    Updated for Campaign component.

---
+++ src/app/home/components/Campaign/Campaign.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Campaign: React.FC = () => {
+  return (
+    <div className="campaign">
+      <div className="container">
+        <div className="headline">
+          <div className="title">Campaigns</div>
+          <div className="description">
+            Discover amazing deals that make your travels unforgettable.
+          </div>
+        </div>
+        <div className="content">Campaign</div>
+      </div>
+    </div>
+  );
+};
+
+export default Campaign;
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,8 +3,10 @@ import Hero from './components/Hero/Hero';
 import ExtraService from './components/ExtraService/ExtraService';
 import CabinClass from './components/CabinClass/CabinClass';
 import AboutUs from './components/AboutUs/AboutUs';
+import Campaign from './components/Campaign/Campaign';
 
 import "./styles/Home.scss";
+
 
 
 const Home: React.FC = () => {
@@ -14,6 +16,7 @@ const Home: React.FC = () => {
       <ExtraService />
       <CabinClass />
       <AboutUs />
+      <Campaign />
     </section>
   );
 };
