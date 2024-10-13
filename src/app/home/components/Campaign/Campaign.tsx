98860d5419c19fa6dd93d0c10095f2dff62efece
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-10-13 16:04:21 +0300

    Update
    Updated Campaign component for Swiper.

--- src/app/home/components/Campaign/Campaign.tsx
+++ src/app/home/components/Campaign/Campaign.tsx
@@ -1,4 +1,11 @@
+"use client";
+
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
 
 const Campaign: React.FC = () => {
   return (
@@ -10,7 +17,25 @@ const Campaign: React.FC = () => {
             Discover amazing deals that make your travels unforgettable.
           </div>
         </div>
-        <div className="content">Campaign</div>
+        <div className="content">
+          <Swiper
+            slidesPerView={4}
+            spaceBetween={20}
+            navigation={true}
+            modules={[Navigation]}
+            className="campaignSwiper"
+          >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide>Slide 5</SwiperSlide>
+            <SwiperSlide>Slide 6</SwiperSlide>
+            <SwiperSlide>Slide 7</SwiperSlide>
+            <SwiperSlide>Slide 8</SwiperSlide>
+            <SwiperSlide>Slide 9</SwiperSlide>
+          </Swiper>
+        </div>
       </div>
     </div>
   );
