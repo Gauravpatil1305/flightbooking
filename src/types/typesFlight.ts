12fde26331a05b9479b611ff1b93b76bb4cdcead
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-10-19 21:37:58 +0300

    Update
    Updated for typesFlight.

--- src/types/typesFlight.ts
+++ src/types/typesFlight.ts
@@ -8,27 +8,3 @@ export interface Flight {
   price: number;
   passengers: number;
 }
-
-export interface RawFlightData {
-  icao24: string;
-  callsign: string;
-  origin_country: string;
-  destination_country?: string | null;
-  time_position?: number | null;
-  last_contact?: number | null;
-  longitude?: number | null;
-  latitude?: number | null;
-  baro_altitude?: number | null;
-  vertical_rate?: number | null;
-  speed?: number | null;
-  heading?: number | null;
-  squawk?: string | null;
-  is_on_ground: boolean;
-  timestamp?: number | null;
-  aircraft_type?: "narrowbody" | "regional" | "widebody";
-  geo_altitude?: number | null;
-}
-
-export interface FlightSearch {
-  states: Flight[]; // Array of flight states
-}
