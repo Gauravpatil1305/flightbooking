96364c3b1daf4cdee453c062e0c855949bd315fd
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-10-25 21:58:35 +0300

    feat: add ManageMyBooking, PNRCode, and PNRSurname components with styling
    - Created ManageMyBooking component for managing user bookings.
    - Developed PNRCode component for PNR code input with floating label design.
    - Implemented PNRSurname component for surname input with floating label design.
    - Integrated PNRCode and PNRSurname components into ManageMyBooking for user input handling.
    - Styled all components for consistent UI/UX and improved user experience.

--- src/app/home/components/FlightDashboard/components/ManageMyBooking/ManageMyBooking.tsx
+++ src/app/home/components/FlightDashboard/components/ManageMyBooking/ManageMyBooking.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from "react";
+import PNRCode from "./components/PNRCode/PNRCode";
+import PNRSurname from "./components/PNRSurname/PNRSurname";
+
+import { Icon } from "@iconify/react";
 
 const ManageMyBooking: React.FC = () => {
+  const [PNRCodeInput, setPNRCodeInput] = useState("");
+  const [surname, setSurname] = useState("");
+
   return (
-    <div>
-        Manage My Booking
+    <div className="manage-booking-wrapper">
+      <div className="manage-booking">
+        <PNRCode
+          PNRCodeInput={PNRCodeInput}
+          setPNRCodeInput={setPNRCodeInput}
+        />
+        <PNRSurname surname={surname} setSurname={setSurname} />
+        <button className="manage-booking-button">
+          <Icon icon="mdi:settings-outline" />
+          Manage Booking
+        </button>
+      </div>
     </div>
   );
 };
