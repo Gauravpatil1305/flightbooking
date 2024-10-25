f2bc99d0ad1ca35efe43a7f693c10766c06354cf
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-10-25 21:36:05 +0300

    feat: add CheckIn and PNRCode components with floating label design and button
    - Added the CheckIn component to handle user check-in process.
    - Integrated the PNRCode component into CheckIn for PNR input.
    - Implemented floating label design for PNRCode input field.
    - Updated PNRCode to accept props for controlled input state.
    - Styled both components to ensure consistent UI/UX.
    - Added a button to CheckIn component for initiating check-in action.

--- src/app/home/components/FlightDashboard/components/CheckIn/CheckIn.tsx
+++ src/app/home/components/FlightDashboard/components/CheckIn/CheckIn.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
+import PNRCode from "./components/PNRCode/PNRCode";
+
+import { Icon } from "@iconify/react";
 
 const CheckIn: React.FC = () => {
+  const [PNRCodeInput, setPNRCodeInput] = useState("");
+
   return (
-    <div>
-        Check-In 
+    <div className="check-in-wrapper">
+      <div className="check-in">
+        <PNRCode
+          PNRCodeInput={PNRCodeInput}
+          setPNRCodeInput={setPNRCodeInput}
+        />
+        <button className="check-in-button">
+          <Icon icon="mdi:check" />
+          Check-In
+        </button>
+      </div>
     </div>
   );
 };
